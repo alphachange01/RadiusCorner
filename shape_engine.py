c19faeeab28530afd97ee9b849093e14151e903b
alphachange01 <alphachange01@gmail.com>
2026-05-02 17:34:42 +0500

    Update shape_engine.py

--- shape_engine.py
+++ shape_engine.py
@@ -4,16 +4,13 @@ import numpy as np
 class ShapeEngine:
 
     @staticmethod
-    def squircle_mask(w, h, radius=30):
+    def squircle_mask(w, h):
         mask = np.zeros((h, w), dtype=np.uint8)
-
-        center = (w // 2, h // 2)
-        axes = (w // 2 - 10, h // 2 - 10)
 
         cv2.ellipse(
             mask,
-            center,
-            axes,
+            (w // 2, h // 2),
+            (w // 2 - 10, h // 2 - 10),
             0,
             0,
             360,
@@ -21,11 +18,4 @@ class ShapeEngine:
             -1
         )
 
-        mask = cv2.GaussianBlur(mask, (7, 7), 0)
-        return mask
-
-    @staticmethod
-    def circle_mask(w, h):
-        mask = np.zeros((h, w), dtype=np.uint8)
-        cv2.circle(mask, (w // 2, h // 2), min(w, h)//2 - 5, 255, -1)
         return cv2.GaussianBlur(mask, (7, 7), 0)
